e15db1ad9d6e73261dc7c88375faf39681ecc60f
raphael <raphael.bideau@gmail.com>
2015-01-06 14:34:51 +0100

    Commentaire sur le fichier de conf

--- config.exemple.php
+++ config.exemple.php
@@ -1,12 +1,13 @@
 <?php
 		
-	$GLOBALS['dns'] = "http://127.0.0.1/path_to_oxygen/";
+	//Url complet du serveur sur lequel est installé l'application web
+	$GLOBALS['dns'] = "http://www.oxygen.com/";
 	
 	//Info base de données
-	$dbhost="localhost";
-	$dbuser="admin";
-	$dbpass="password";
-	$dbname="deep_blue";
+	$dbhost="localhost";	// Hôte de la base de données
+	$dbuser="admin";		// Nom de connection à la base de données
+	$dbpass="password";		// Mot de passe de connection à la base de données
+	$dbname="deep_blue";	// Nom de la base de données
 
 	//Nombre de plongeur suggéré dans le formulaire d'édition
 	$GLOBALS['nombre_plongeur_suggerer'] = 15;
@@ -16,15 +17,14 @@
 	$GLOBALS['plongeDefaultDuree'] = 40;
 
 	//Info envoi de mail
-	$GLOBALS['email']['nom_expediteur'] = "Oxygen";
-	$GLOBALS['email']['adresse_expediteur'] = "contact@oxygen.com";
-	$GLOBALS['duree_validite_lien_reinitialisation'] = 24*60*60; //en seconde, 24h pour l'instant
-	$GLOBALS['nom_application'] = "Oxygen";
+	$GLOBALS['email']['nom_expediteur'] = "Oxygen";					// Nom de l'expediteur des mails
+	$GLOBALS['email']['adresse_expediteur'] = "contact@oxygen.com";	// Addresse de l'expedition des mails
+	$GLOBALS['duree_validite_lien_reinitialisation'] = 24*60*60; 	// Durée de validité des liens de réinisialisation de mot de passe, en seconde (24h par défaut)
+	$GLOBALS['nom_application'] = "Oxygen";							// Nom de l'application et interface web
 
-	error_reporting(E_ALL);
-	ini_set('display_errors','1'); 
-	ini_set("log_errors", '1');
-	ini_set("error_log", dirname(__FILE__).DIRECTORY_SEPARATOR."log".DIRECTORY_SEPARATOR."erreur.txt");
-	
-	$force_css_compile= false;
+	//Gestion des erreurs et affichage
+	error_reporting(E_ALL);			// Affichage de toute les erreurs (Constante PHP)
+	ini_set('display_errors','1'); 	// Affichages des erreurs à l'utilisateur ('1' pour activer, '0' pour désactiver)
+	ini_set("log_errors", '1');		// Enregistrement des erreurs dans un fichier de log ('1' pour activer, '0' pour désactiver)
+	ini_set("error_log", dirname(__FILE__).DIRECTORY_SEPARATOR."log".DIRECTORY_SEPARATOR."erreur.txt"); // Chemin du fichier de log relatif à ce fichier de configuration
 ?>
